a91920643ce6b0964bd4031d6c36e46371ca850e
Johan <s196763@student.pg.edu.pl>
2023-06-03 16:31:42 +0200

    Add some images of CMAKE and the installation scripts for Realsense SDK

---
+++ SDK_Installation/realsense_installation.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -xe
+
+#Locally suppress stderr to avoid raising not relevant messages
+exec 3>&2
+exec 2> /dev/null
+con_dev=$(ls /dev/video* | wc -l)
+exec 2>&3
+
+if [ $con_dev -ne 0 ];
+then
+	echo -e "\e[32m"
+	read -p "Remove all RealSense cameras attached. Hit any key when ready"
+	echo -e "\e[0m"
+fi
+
+lsb_release -a
+echo "Kernel version $(uname -r)"
+sudo apt-get update
+cd ~/
+sudo rm -rf ./librealsense_build
+mkdir librealsense_build && cd librealsense_build
+
+if [ $(sudo swapon --show | wc -l) -eq 0 ];
+then
+	echo "No swapon - setting up 1Gb swap file"
+	sudo fallocate -l 2G /swapfile
+	sudo chmod 600 /swapfile
+	sudo mkswap /swapfile
+	sudo swapon /swapfile
+	sudo swapon --show
+fi
+
+echo Installing Librealsense-required dev packages
+sudo apt-get install git cmake libssl-dev freeglut3-dev libusb-1.0-0-dev pkg-config libgtk-3-dev unzip -y
+
+wget https://github.com/IntelRealSense/librealsense.git
+cd ./librealsense
+
+echo Install udev-rules
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
+sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger 
+echo Install udev-rules
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
+sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger 
+mkdir build && cd build
+sudo make uninstall && make clean && sudo make && sudo make install
+
+echo -e "\e[92m\n\e[1mLibrealsense script completed.\n\e[0m"
+
+cmake ../ -DBUILD_EXAMPLES=true -DBUILD_GRAPHICAL_EXAMPLES=false -DBUILD_PYTHON_BINDINGS=true -DBUILD_SHARED_LIBS=false -DPYTHON_EXECUTABLE=/usr/bin/python3 -DBUILD_NETWORK_DEVICE=ON -DFORCE_RSUSB_BACKEND=ON -DCMAKE_BUILD_TYPE=Release BUILD_PYTHON_DOCS
