4d63b08252f1ca087c0e295225add92fd857a4b2
Andrii Melnyk <nenajyja@gmail.com>
2023-06-04 19:36:28 +0300

    demo program from rpi

--- arm/arm_microcode.py
+++ arm/arm_microcode.py
@@ -9,6 +9,7 @@ command = None
 cobot = None
 error_message= str()
 message = None
+timeout = 0
 
 def screen_init():
     setScreenColor(0x222222)
@@ -17,7 +18,7 @@ def screen_init():
     lcd.font(lcd.FONT_Ubuntu)
     lcd.print('Listening UART Commands', 0, 0, 0xffffff)
 
-def parse_command(uart_in, robot):
+def parse_command(uart_in, robot, uart_write_func):
     try:
         command = str(uart_in).split(' ')
         if command[0] == 'power':
@@ -29,6 +30,45 @@ def parse_command(uart_in, robot):
                 return 'power off'
             else:
                 return 'power is ' + 'on' if robot.is_power_on() else 'off'
+        elif command[0] == 'start_position':
+            uart_write_func('move back to start position....\r\n')
+            robot.send_angles([0,0,0,0,0,0], 10)
+            timeout = 0
+            while robot.is_moving():
+                timeout += 1
+                if timeout > 100:
+                    uart_write_func('movement toooo long\r\n')
+                    break
+                wait_ms(100)
+            return 'retruned to start position [0,0,0,0,0,0], 10 done.'
+        elif command[0] == 'get_position':
+            return 'angles ' + str(robot.get_angles())
+        elif command[0] == 'set_angle':
+            uart_write_func('move ' + str(command[1]) + ' joint to angle ' + str(command[2]) + '...\r\n')
+            #robot.send_angle(int(command[1]), int(command[2]), 10)
+            robot.jog_angle(int(command[1]), int(command[2]), 10)
+            return 'moved ' + str(command[1]) + ' joint to start angle ' + str(command[2]) + ' done.'
+        elif command[0] == 'get_joint_maxmin':
+            return '1: [' + str(robot.get_joint_min_angle(1)) + ':' + str(robot.get_joint_max_angle(1)) + '] ' + \
+                '2: [' + str(robot.get_joint_min_angle(2)) + ':' + str(robot.get_joint_max_angle(2)) + '] ' + \
+                '3: [' + str(robot.get_joint_min_angle(3)) + ':' + str(robot.get_joint_max_angle(3)) + '] ' + \
+                '4: [' + str(robot.get_joint_min_angle(4)) + ':' + str(robot.get_joint_max_angle(4)) + '] ' + \
+                '5: [' + str(robot.get_joint_min_angle(5)) + ':' + str(robot.get_joint_max_angle(5)) + '] ' + \
+                '6: [' + str(robot.get_joint_min_angle(6)) + ':' + str(robot.get_joint_max_angle(6)) + ']\r\n'
+        elif command[0] == 'get_is_moving':
+            return 'True\r\n' if robot.is_moving() else 'False\r\n'
+        elif command[0] == 'set_position':
+            uart_write_func('move to position....')
+            robot.send_angles([int(command[1]),int(command[2]),int(command[3]),int(command[4]),int(command[5]),int(command[6])],int(command[7]))
+            timeout = 0
+            while robot.is_moving():
+                timeout += 1
+                if timeout > 30:
+                    uart_write_func('movement toooo long\r\n')
+                    break
+                wait_ms(100)
+            return 'position ' + str(robot.get_angles()) + ' done.'
+        
     except Exception as e:
         return 'Error during command: ' + str(e)
 
@@ -45,7 +85,7 @@ def main_run():
         if uart2.any():
             wait_ms(100)
             message = uart2.readline().decode()
-            response = parse_command(message, cobot)
+            response = parse_command(message, cobot, uart2.write)
             #uart2.write('echo ')
             #uart2.write(message)
             uart2.write(str(response) + "\r\n") 
--- arm/arm_move.py
+++ arm/arm_move.py
@@ -1,11 +1,53 @@
 import serial
 from time import sleep
 
+'''
+b'get_position is\r\n',
+b'set_angle 1 0\r\n',
+b'set_angle 2 0\r\n',
+b'set_angle 3 30\r\n',
+b'set_angle 4 -30\r\n',
+b'set_angle 5 30\r\n',
+b'get_is_moving is\r\n',
+b'get_position is\r\n',
+b'set_position 10 0 -15 15 -15 15 10\r\n',
+b'get_position is\r\n',
+b'start_position is\r\n',
+'''
+
 command_list = [
     b'power is\r\n',
     b'power on\r\n',
     b'power is\r\n',
+    b'get_position is\r\n',
+    b'start_position is\r\n',
+    b'get_position is\r\n',
+    b'set_angle 1 30 \r\n',
+    #b'set_angle 2 30 \r\n',
+    #b'set_angle 3 10 \r\n',
+    #b'set_angle 4 10 \r\n',
+    b'set_angle 5 90 \r\n',
+    b'set_angle 6 90 \r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'get_position is\r\n',
+    b'set_position 10 0 0 0 0 0 10 \r\n',
+    b'get_position is\r\n',
+    b'start_position is\r\n',
+    b'get_joint_maxmin is\r\n',
     b'power off\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
+    b'power is\r\n',
     b'power is\r\n',
 ]
 
@@ -14,6 +56,7 @@ for command in command_list:
     ser.write(command)
     sleep(1)
     received_data = ser.readline()
+    #received_data = ser.readlines()
     print(received_data)
     sleep(1)
 
